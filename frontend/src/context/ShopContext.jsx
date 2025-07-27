579622ee1da5f15a0e2e3a321dff7302ee0f90c1
Harine77 <harine2310403@ssn.edu.in>
2025-07-27 11:51:48 +0530

    Added best seller,latestcollection,footer

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,12 +1,19 @@
 import { createContext } from "react";
+import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = () =>{
-    const value = {}
+const ShopContextProvider = (props) =>{
+    const currency = '₹';
+    const delivery_fee = 10;
+
+    const value = {
+        products,currency,delivery_fee
+    }
     return (
-        <ShopContext.Provider>
-            {props.childern}
+        <ShopContext.Provider value={value}>
+            {props.children}
         </ShopContext.Provider>
     )
 }
+export default ShopContextProvider
